ebf7b096c77a4102b21fd1b3d9f9bc2199513114
Marcelmtb2 <mtb2@softex.cin.ufpe.br>
2025-01-07 15:32:05 -0300

    [TEST] Remove example code
    Removing the example code from the library documentation

--- statemachine.py
+++ statemachine.py
@@ -49,42 +49,6 @@ class ObjectTracking(object):
         # Service not recognizing the image (needs object repositioning)
         # 
 
-        # Superheroes need to keep in shape.
-        self.machine.add_transition('work_out', 'hanging out', 'hungry')
-
-        # Those calories won't replenish themselves!
-        self.machine.add_transition('eat', 'hungry', 'hanging out')
-
-        # Superheroes are always on call. ALWAYS. But they're not always
-        # dressed in work-appropriate clothing.
-        self.machine.add_transition('distress_call', '*', 'saving the world',
-                         before='change_into_super_secret_costume')
-
-        # When they get off work, they're all sweaty and disgusting. But before
-        # they do anything else, they have to meticulously log their latest
-        # escapades. Because the legal department says so.
-        self.machine.add_transition('complete_mission', 'saving the world', 'sweaty',
-                         after='update_journal')
-
-        # Sweat is a disorder that can be remedied with water.
-        # Unless you've had a particularly long day, in which case... bed time!
-        self.machine.add_transition('clean_up', 'sweaty', 'asleep', conditions=['is_exhausted'])
-        self.machine.add_transition('clean_up', 'sweaty', 'hanging out')
-
-        # Our NarcolepticSuperhero can fall asleep at pretty much any time.
-        self.machine.add_transition('nap', '*', 'asleep')
-
-    def update_journal(self):
-        """ Dear Diary, today I saved Mr. Whiskers. Again. """
-        self.kittens_rescued += 1
-
-    @property
-    def is_exhausted(self):
-        """ Basically a coin toss. """
-        return random.random() < 0.5
-
-    def change_into_super_secret_costume(self):
-        print("Beauty, eh?")
 
 if __name__ == "__main__":
     batman = ObjectTracking("Camera")
